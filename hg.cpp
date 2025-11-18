c29fc4145db949a74c12abc70a571532c8483f7d
Hannah <78294063+hanszuki@users.noreply.github.com>
2025-11-18 13:28:51 -0400

    miau miau miau

--- hg.cpp
+++ hg.cpp
@@ -1,1 +1,15 @@
-putooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    while (1==1)
+    {
+           cout << "Miau jueguen lol miau" << std::endl;
+    }
+    
+
+    
+    return 0;
+}
